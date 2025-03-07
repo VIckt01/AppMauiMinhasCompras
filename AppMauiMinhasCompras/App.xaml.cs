72dba92c4732471ba4c6d15824628609473db34c
victo <victoriagabriellecamargo@gmail.com>
2025-03-06 21:45:37 -0300

    Adicionar arquivos de projeto.

---
+++ AppMauiMinhasCompras/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMauiMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
